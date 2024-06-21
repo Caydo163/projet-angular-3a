5b7ee7f296f9541ab42f15652aade843e7bb557e
justin.carvalheiro <justin.carvalheiro16@gmail.com>
2024-06-21 16:33:03 +0200

    💄 navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, MatButton],
+  imports: [RouterLink, MatButton, MatIconModule, MatToolbarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
